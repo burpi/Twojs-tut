1b1baacc3a2a921b197f3b5ea0a9c44f88615b90
burpi <c.faulmann1988@gmail.com>
2017-09-28 22:04:16 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+
+
+var elem = document.getElementById('shapes'),
+    two = new Two({
+      fullscreen: true
+    });
+    two.appendTo(elem);
+
+var earthAngle = 0,
+    moonAngle = 0,
+    distance = 30,
+    radius = 50,
+    padding = 100,
+    orbit = 200,
+    offset = orbit + padding,
+    orbits = two.makeGroup();
+
+var earthOrbit = two.makeCircle(offset, offset, orbit);
+earthOrbit.noFill();
+earthOrbit.linewidth = 4;
+earthOrbit.stroke = "#ccc";
+orbits.add(earthOrbit);
+
+function getPositions(angle, orbit){
+  return{
+    x: Math.cos(angle * Math.PI / 180) * orbit,
+    y: Math.sin(angle * Math.PI / 180) * orbit
+  };
+}
+
+var pos = getPositions(earthAngle++, orbit),
+    earth = two.makeCircle(pos.x + offset, pos.y + offset, radius);
+
+    earth.stroke = "#123456";
+    earth.linewidth = 4;
+    earth.fill = "#194878";
+
+var moonOrbit = two.makeCircle(earth.translation.x, earth.translation.y, radius + distance);
+moonOrbit.noFill();
+moonOrbit.linewidth = 4;
+moonOrbit.stroke = "#ccc";
+orbits.add(moonOrbit);
+
+var pos = getPositions(moonAngle, radius + distance),
+    moon = two.makeCircle(earth.translation.x + pos.x, earth.translation.y + pos.y, radius / 4);
+
+    moonAngle += 5;
+    moon.fill = "#474747";
+
+
+two.bind("update", function (framecount) {
+  var pos = getPositions(earthAngle++, orbit);
+  var moonPos = getPositions(moonAngle, radius + distance);
+  earth.translation.x = pos.x + offset;
+  earth.translation.y = pos.y + offset;
+  moon.translation.x = earth.translation.x + moonPos.x;
+  moon.translation.y = earth.translation.y + moonPos.y;
+  moonAngle += 5;
+  moonOrbit.translation.x = earth.translation.x;
+  moonOrbit.translation.y = earth.translation.y;
+});
+
+
+
+
+two.play();
+two.update();
